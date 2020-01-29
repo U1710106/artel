276968500ee97ad81b217fda9a779172316835e3
Azizbek <a.begmurodovstudent@gmail.com>
2020-01-29 12:02:42 +0900

    Form fields created

--- index.php
+++ index.php
@@ -5,6 +5,22 @@
 </head>
 <body>
 
+<div class="form-style-10">
+    <h1>Client Registration</h1>
+    <form>
+        <div class="section">First Name & Last Name</div>
+        <div class="inner-wrap">
+            <label>First Name <input type="text" name="name" /></label>
+            <label>Last Name <input type="text" name="surname" /></label>
+        </div>
+
+        <div class="section">Birthday & Phone</div>
+        <div class="inner-wrap">
+            <label>Birthday <input type="date" name="birthday" /></label>
+            <label>Phone Number <input type="text" name="phone" /></label>
+        </div>
+    </form>
+</div>
 
 </body>
 </html>
